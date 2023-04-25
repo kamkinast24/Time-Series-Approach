fa32e4761408effcbf3edef4df48d5eea38603fe
kamkinast24 <113359314+kamkinast24@users.noreply.github.com>
2023-04-25 14:38:18 -0400

    Create Storm-Events-Data-Prep.R

---
+++ Vector Autoregression/Data Preprocessing/Storm-Events-Data-Prep.R
@@ -0,0 +1,22 @@
+#Data Wrangling with Storm Event Database
+library(dplyr)
+library(lubridate)
+
+#Storm Events
+SE_df <- read.csv(file = 'storm_events.csv')
+#Convert into datetime
+SE_df <- SE_df %>%
+  mutate(DATE = lubridate::mdy(BEGIN_DATE))
+
+#Filter to three columns only
+#SE_df<- SE_df %>% select(c('DATE',  'DEATHS_DIRECT', 'INJURIES_DIRECT'))
+
+#sum all duplciate rows
+SE_df <- SE_df %>% 
+group_by(DATE) %>%
+summarise(deaths = sum(DEATHS_DIRECT),
+          injuries = sum(INJURIES_DIRECT))
+
+#export as csv file
+library(readr)
+write_csv(SE_df, "Reduced_Storm_CSV.csv")

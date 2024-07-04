d7411add6d8f37b118fab65f75b5339ef82655b8
Kam Kinast <113359314+kamkinast24@users.noreply.github.com>
2024-07-04 19:44:17 -0400

    Update Gaussian_Processes.jl

--- Time Series Forecasting/Gaussian_Processes.jl
+++ Time Series Forecasting/Gaussian_Processes.jl
@@ -8,19 +8,19 @@ using DataFrames
 kernel = Noise(1.0) + Periodic(0.0,1.0,1.0) + SE(4.0,0.0)
 
 #Maximum Temp
-gp_max = GP(x_train_max,y_train_max, MeanZero(),kernel)   #Fit the GP
+gp = GP(x_train,y_train, MeanZero(),kernel)   #Fit the GP
 
-optimize!(gp_max) #Estimate the parameters through maximum likelihood estimation
+optimize!(gp) #Estimate the parameters through maximum likelihood estimation
 
-μ_max, Σ_max = predict_y(gp_max,x_test_max);
+μ, Σ = predict_y(gp,x_test);
 
 using Plots
 pyplot()
 
-plot(x_test_max,μ_max,ribbon=Σ_max)
+plot(x_test,μ,ribbon=Σ)
 
 #Evaluate the model performance
 using MLJ
 
-println(rms(μ_max, y_test_max)) #RMSE
-println(mae(μ_max, y_test_max)) #MAE
+println(rms(μ_max, y_test)) #RMSE
+println(mae(μ_max, y_test)) #MAE

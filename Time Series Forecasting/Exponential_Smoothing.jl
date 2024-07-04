affd2e19a230a50cd55bfccb606d2aae280bb139
Kam Kinast <113359314+kamkinast24@users.noreply.github.com>
2024-07-04 19:42:56 -0400

    Update Exponential_Smoothing.jl

--- Time Series Forecasting/Exponential_Smoothing.jl
+++ Time Series Forecasting/Exponential_Smoothing.jl
@@ -3,19 +3,19 @@ using StateSpaceModels
 #Exponential Smoothing using Houston's Maximum Temperature
 
 #Create ES model based on y trainset
-model_ets_max = StateSpaceModels.ExponentialSmoothing(Vector{Float64}(y_train_max); trend = false, seasonal = 16) #This model only accepts vector of float64
+model_ets= StateSpaceModels.ExponentialSmoothing(Vector{Float64}(y_train); trend = false, seasonal = 16) #This model only accepts vector of float64
 #Fit the model
-StateSpaceModels.fit!(model_ets_max)
+StateSpaceModels.fit!(model_ets)
 
 #Forecasting the exponential smoothing
-forec_ets_max= StateSpaceModels.forecast(model_ets_max, length(y_test_max))
+forec_ets= StateSpaceModels.forecast(model_ets, length(y_test))
   
 #Plot the forecast
-plot(model_ets_max, forec_ets_max)
+plot(model_ets, forec_ets)
 
 #Evaluate the model performance
 using MLJ
-forecast_ets_max = reduce(vcat,forec_ets_max.expected_value)
+forecast_ets = reduce(vcat,forec_ets_max.expected_value)
 
-println(rms(forecast_ets_max, y_test_max)) #RMSE
-println(mae(forecast_ets_max, y_test_max)) #MAE
+println(rms(forecast_ets, y_test_max)) #RMSE
+println(mae(forecast_ets, y_test_max)) #MAE

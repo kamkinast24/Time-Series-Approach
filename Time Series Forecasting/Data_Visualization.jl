7c813bedfa763814dee808f6f665a4f97c3adf78
Kam Kinast <113359314+kamkinast24@users.noreply.github.com>
2024-07-04 21:06:44 -0400

    Update Data_Visualization.jl

--- Time Series Forecasting/Data_Visualization.jl
+++ Time Series Forecasting/Data_Visualization.jl
@@ -38,3 +38,49 @@ ylims!(-40, 30) #minimum
 yticks!([ -30, -20,-10, 0, 10, 20, 30]) #minimum
 
 png("Chicago_Minimum_Temp.png")
+
+
+using Plots; using CSV; using DataFrames; using Statistics; using GLM
+
+#Extract the dataset of MLP RESULTS
+mlp_forecast = DataFrame(CSV.File("y_forecast.csv"))
+MLP_forecast_data = last(mlp_forecast.values, 2922)
+
+#Collect the dates
+dr = Dates.Date(2003,1,1):Day(1):Date(2030,12,31)
+num_day_combined = collect(dr)
+dr_obs = Dates.Date(2003,1,1):Day(1):Date(2022,12,31)
+num_day_obs = collect(dr_obs)
+dr_forecast = Dates.Date(2023,1,1):Day(1):Date(2030,12,31)
+num_day_for = collect(dr_forecast)
+
+#Convert to Celisus
+MLP_forecast_data_C = (MLP_forecast_data.-32.0).* (5/9)
+#observations = (main_df.TMAX.-32.0).* (5/9) #maximum
+observations = (main_df.TMIN.-32.0).* (5/9) #minimum
+
+#Combine observations and MLP
+combination = reduce(vcat,(observations, MLP_forecast_data_C))
+
+#trend 
+#Adding number of days
+df_proejction = DataFrame(DAY = 1.0:length(combination), VALUES = combination)
+model = lm(@formula(VALUES ~ DAY), df_proejction)
+trend = 0.000213184.*df_proejction.DAY.+ 4.81406
+
+plot(num_day_combined,combination, label = false, framestyle = :box)
+plot!(num_day_obs,observations, label = "Observations", lw = 3)
+plot!(num_day_for, MLP_forecast_data_C, label = "MLP Forecast Values", lw = 3)
+plot!(num_day_combined, trend, label = "Trendline", color = "grey0", lw = 3, legend =:bottomright)
+#annotate!(num_day_combined[1195], -25,text("y = 0.0002x + 4.81", 7)) #maximum
+annotate!(num_day_combined[1195], -35,text("y = 0.0002x + 4.81", 7)) #minimum
+ylabel!("Minimum Temperature [°C]")
+#ylabel!("Maximum Temperature [°C]")
+xlabel!("Dates")
+#ylims!(-30, 40) #maximum
+ylims!(-40, 30) #minimum
+#yticks!([-20, -10, 0,10,20,30,40]) #maximum
+yticks!([ -30, -20,-10, 0, 10, 20, 30]) #minimum
+
+
+png("Chicago_Minimum_Forecast.png")
